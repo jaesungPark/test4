906e5acb7aa7d56c748b8ecc44c9ca53270d342e
jaesungPark <hotpjs1@gmail.com>
2012-06-02 17:15:29 +0900

    test

---
+++ test4/src/com/jason/test4/Test4Activity.java
@@ -0,0 +1,14 @@
+package com.jason.test4;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Test4Activity extends Activity {
+    /** Called when the activity is first created. */
+	//
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
